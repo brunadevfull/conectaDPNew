c87ce7f78293cb25ef70830050959487d69312d4
brunadevfull <brunadev.full@example.com>
2025-04-08 12:15:29 -0300

    adicionando css

---
+++ app/Views/layout.php
@@ -0,0 +1,14 @@
+<!-- app/Views/layout.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- CSS Globais -->
+    <link rel="stylesheet" type="text/css" href="/conectaPapem/css/global.css">
+    
+    <!-- CSS Específicos por página -->
+    <?php echo $additionalCSS ?? ''; ?>
+</head>
+<body>
+    <!-- Conteúdo -->
+</body>
+</html>
